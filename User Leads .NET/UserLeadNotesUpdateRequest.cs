0ad30d40798a17c64eabb88200a13607745caf53
Orion Lozano <133153123+OrionLoz@users.noreply.github.com>
2023-09-21 12:14:38 -0700

    Update UserLeadNotesUpdateRequest.cs

--- User Leads .NET/UserLeadNotesUpdateRequest.cs
+++ User Leads .NET/UserLeadNotesUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests
+namespace MoneFi.Models.Requests
 {
     public class UserLeadNotesUpdateRequest
     {
