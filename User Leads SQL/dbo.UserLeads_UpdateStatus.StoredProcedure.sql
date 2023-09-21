48e280277a82d203ae83519bf10bcde060301259
Orion Lozano <133153123+OrionLoz@users.noreply.github.com>
2023-09-21 12:10:41 -0700

    Update dbo.UserLeads_UpdateStatus.StoredProcedure.sql

--- User Leads SQL/dbo.UserLeads_UpdateStatus.StoredProcedure.sql
+++ User Leads SQL/dbo.UserLeads_UpdateStatus.StoredProcedure.sql
@@ -1,17 +1,10 @@
-CREATE proc [dbo].[UserLeads_UpdateStatus]
-				@Id int,
-				@StatusId int
-
-as
-
+CREATE PROCEDURE [dbo].[UserLeads_UpdateStatus]
+    @Id int,
+    @StatusId int
+AS
 BEGIN
-
-
-UPDATE dbo.UserLeads
-SET    [StatusId] = @StatusId,
-       [DateModified] = GETUTCDATE()
-
-WHERE [Id] = @Id
-
-
-END
+    UPDATE dbo.UserLeads
+    SET [StatusId] = @StatusId,
+        [DateModified] = GETUTCDATE()
+    WHERE [Id] = @Id;
+END;
