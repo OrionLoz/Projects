ab2f6b0633dc7d325f334cbcd8d6a2902f268cc2
Orion Lozano <133153123+OrionLoz@users.noreply.github.com>
2023-09-21 12:08:50 -0700

    Update dbo.UserLeads_SelectAll.StoredProcedure.sql

--- User Leads SQL/dbo.UserLeads_SelectAll.StoredProcedure.sql
+++ User Leads SQL/dbo.UserLeads_SelectAll.StoredProcedure.sql
@@ -1,40 +1,30 @@
-
-CREATE proc [dbo].[UserLeads_SelectAll]
-			 @PageIndex int
-			 ,@PageSize int
-
-as
-
-
+CREATE PROCEDURE [dbo].[UserLeads_SelectAll]
+    @PageIndex int,
+    @PageSize int
+AS
 BEGIN
+    DECLARE @offset int = @PageIndex * @PageSize;
 
-DECLARE @offset int = @PageIndex * @PageSize
-	
-		SELECT ul.[Id]
-			  ,ul.[Email]
-			  ,ul.[FirstName]
-			  ,ul.[LastName]			  
-			  ,ul.[LoanAmount]			  
-			  ,ul.[LoanTypeId] 
-			  ,lt.Name as LoanTypeName 
-			  ,ul.[StatusId]
-			  ,st.Name as StatusTypeName
-			  ,lt.Description
-			  ,ul.[DateCreated]
-			  ,ul.[DateModified]
-			  ,ul.Notes
-			  ,TotalCount = COUNT(1) OVER()
-
-		FROM [dbo].[UserLeads] as ul 
-		inner join dbo.LoanTypes as lt 
-		on ul.LoanTypeId=lt.Id
-		inner join dbo.StatusTypes as st 
-		on ul.StatusId = st.Id
-
-		ORDER BY ul.Id
-
-		OFFSET @offset Rows
-		Fetch Next @PageSize Rows ONLY
-
-END
-GO
+    SELECT ul.[Id]
+        ,ul.[Email]
+        ,ul.[FirstName]
+        ,ul.[LastName]
+        ,ul.[LoanAmount]
+        ,ul.[LoanTypeId]
+        ,lt.Name as LoanTypeName
+        ,ul.[StatusId]
+        ,st.Name as StatusTypeName
+        ,lt.Description
+        ,ul.[DateCreated]
+        ,ul.[DateModified]
+        ,ul.Notes
+        ,TotalCount = COUNT(1) OVER()
+    FROM [dbo].[UserLeads] as ul
+    INNER JOIN dbo.LoanTypes as lt
+        ON ul.LoanTypeId = lt.Id
+    INNER JOIN dbo.StatusTypes as st
+        ON ul.StatusId = st.Id
+    ORDER BY ul.Id
+    OFFSET @offset ROWS
+    FETCH NEXT @PageSize ROWS ONLY;
+END;
